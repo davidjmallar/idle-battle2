2a8afc41255c38d90cbb2a5462a01e3e84406714
davidjmallar <david.mallar@tutanota.com>
2023-12-25 07:30:31 +0100

    Enemy Spawning

--- Assets/Scripts/CreatureSpawner.cs
+++ Assets/Scripts/CreatureSpawner.cs
@@ -10,18 +10,21 @@ public class CreatureSpawner : MonoBehaviour
 
 	private readonly List<CreatureController> _heroTeam = new List<CreatureController>();
 	private readonly List<CreatureController> _foeTeam = new List<CreatureController>();
-
+	private int _index = 3;
 	[Button]
 	public void SpawnDummyHeroTeam()
 	{
 		SpawnHeroTeam(DataService.GetSaveData().Creatures);
 	}
+	[Button]
+	public void SpawnDummyEnemy()
+	{
+		SpawnEnemy(_index++);
+	}
 	public void SpawnEnemy(int index)
 	{
-		var enemy = new Creature();
-		
-		_foeTeam.Add(SpawnCreature(enemy, enemy.PositionInGroup, isEnemy: true));
-
+		var enemy = SpawnCreature(1, index);
+		_foeTeam.Add(SpawnCreature(enemy, enemy.PositionInMap, isEnemy: true));
 	}
 
 	public void SpawnHeroTeam(List<Creature> Creatures)
@@ -44,5 +47,11 @@ public class CreatureSpawner : MonoBehaviour
 		_heroTeam.ForEach(h => h.Feed(deltaT));
 		_foeTeam.ForEach(h => h.Feed(deltaT));
 	}
-
+	public Creature SpawnCreature(int level, int index)
+	{
+		var enemy = new Creature();
+		enemy.PositionInMap = new Vector3Int(index, Random.Range(0, -3), 0);
+		enemy.CreatureId = "sprite_sheet_pikeman_0_16x16";
+		return enemy;
+	}
 }
